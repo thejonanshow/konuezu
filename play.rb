21eb8e9823f84c8fb8f63a724fc45a7c965fa2f0
Jonan Scheffler <jonanscheffler@gmail.com>
2018-05-31 16:21:45 +0900

    KHALEEEEEEEEEEESI

--- play.rb
+++ play.rb
@@ -5,5 +5,20 @@ require 'rspec'
 # live with > 3, die
 # dead with == 3, spawn
 
+class Biome
+  attr_reader :year
+
+  def initialize
+    @year = 0
+  end
+end
+
 RSpec.describe 'Conway' do
+  context Biome do
+    let(:biome) { Biome.new }
+
+    it "starts at year 0" do
+      expect(biome.year).to eq(0)
+    end
+  end
 end
