589b96ba624e0961eb5e7fe0dc5fec6b45f115bf
Jonan Scheffler <jonanscheffler@gmail.com>
2018-06-01 14:42:21 +0900

    Thank you Aaron for cheating...

--- play.rb
+++ play.rb
@@ -1,9 +1,30 @@
-require 'rspec'
-
 # live < 2, die
 # live 2/3, live
 # live with > 3, die
 # dead with == 3, spawn
 
-RSpec.describe 'Conway' do
+require 'game_of_life'
+
+class Cell
+  def initialize
+    @state = false
+  end
+
+  def alive?
+    @state
+  end
+
+  def dead?; !alive? end
+
+  def should_die?
+    case neighbors.length
+    when 0, 1
+      true
+    else
+      false
+    end
+  end
+end
+
+class World
 end
