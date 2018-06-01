bf2fee5120749469235d588918c156ccd72595ee
Jonan Scheffler <jonanscheffler@gmail.com>
2018-06-01 16:44:21 +0900

    Thank you Matz!

--- play.rb
+++ play.rb
@@ -1,9 +1,41 @@
-require 'rspec'
 
-# live < 2, die
-# live 2/3, live
-# live with > 3, die
-# dead with == 3, spawn
+size = 20
+board = Array.new(size*size) { rand(2) }
 
-RSpec.describe 'Conway' do
+loop do
+  board.each.with_index { |cell, index| print "\n" if index % size == 0; print cell }
+
+  board = board.map.with_index do |cell, index|
+    sum = 0
+    n=index-size
+    s=index+size
+    w=index-1
+    e=index+1
+
+    sum += board[n] || 0
+    sum += board[s] || 0
+    sum += board[w] || 0
+    sum += board[e] || 0
+    sum += board[n-1] || 0
+    sum += board[n+1] || 0
+    sum += board[s-1] || 0
+    sum += board[s+1] || 0
+
+    if board[index] == 0 # dead
+      if sum == 3
+        1
+      else
+        0
+      end
+    else # live
+      if sum < 2 || sum > 3
+        0
+      else
+        1
+      end
+    end
+  end
+
+  sleep 0.1
+  puts "\n" * 100
 end
